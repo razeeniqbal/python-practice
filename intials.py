385430275469ed0140ef72ff7b635e271a4e5918
Razeen Iqbal <117481105+razeeniqbal@users.noreply.github.com>
2024-03-18 15:26:56 +0800

    Add files via upload

---
+++ intials.py
@@ -0,0 +1,10 @@
+#i love to code and i keep eager to code by myself. no matter how hard i tried, i will success one day!
+
+print("RRRR     IIIII  ")
+print("R       R     I  ")
+print("R       R     I  ")
+print("RRRR        I  ")
+print("R R            I  ")
+print("R   R          I  ")
+print("R     R        I  ")
+print("R       R    IIIII  ")
